e20593d0a04a849cc9205d9f83f97e87801ad054
Ethan Furman <ethan@stoneleaf.us>
2014-08-13 18:35:03 -0700

    move actual links to an untracked file

---
+++ links.py
@@ -0,0 +1,10 @@
+# copy this file to _links.py, and complete
+
+salesinq_links = [
+    ('link_name','link_text',
+        "salesinq paramaters (%s for the item/customer/whatever id)"),
+    (..., ..., ...),
+    ]
+
+
+
--- product.py
+++ product.py
@@ -3,25 +3,7 @@ from fnx import xid, dynamic_page_stub, static_page_stub
 from osv import osv, fields
 from urllib import urlopen
 
-salesinq_links = [
-    ('salesinq_allyrsmos','Years&nbsp;&&nbsp;Months',
-        "pyvotRpt?ck_qty=on&optX_op=Item&Item_op=%s&rptColFmt_op=allyrsmos"),
-    ('salesinq_allyrsmos_rep','Years&nbsp;&&nbsp;Months&nbsp;by&nbsp;Rep',
-        "pyvotRpt?ck_qty=on&Item_op=%s&rptColFmt_op=allyrsmos&optX_op=Rep"),
-    ('salesinq_allyrsmos_shipto','Years&nbsp;&&nbsp;Months&nbsp;by&nbsp;ShipTo',
-        "pyvotRpt?ck_qty=on&Item_op=%s&rptColFmt_op=allyrsmos&optX_op=ShipTo"),
-    ('salesinq_allyears_rep','Years&nbsp;by&nbsp;Rep',
-        "pyvotRpt?ck_descs=on&ck_qty=on&Item_op=%s&rptColFmt_op=allyears&optX_op=Rep"),
-    ('salesinq_allyrs_shipto','Years&nbsp;by&nbsp;ShipTo',
-        "pyvotRpt?ck_descs=on&ck_qty=on&Item_op=%s&rptColFmt_op=allyears&optX_op=ShipTo"),
-    ('salesinq_allyrs_repcust','Years&nbsp;by&nbsp;Rep/Customer',
-        "pyvotRpt?ck_descs=on&ck_qty=on&Item_op=%s&rptColFmt_op=allyears&optX_op=Rep&optY_op=Cust"),
-    ('salesinq_allyrs_statecity','Years&nbsp;by&nbsp;State/City',
-        "pyvotRpt?ck_descs=on&ck_qty=on&Item_op=%s&rptColFmt_op=allyears&optX_op=State&optY_op=City"),
-    ('salesinq_allyrs_citycust','Years&nbsp;by&nbsp;City/Customer',
-        "pyvotRpt?ck_descs=on&ck_qty=on&Item_op=%s&rptColFmt_op=allyears&optX_op=City&optY_op=Cust"),
-    ]
-
+from _links import *
 
 def salesinq(obj, cr, uid, ids, fields, arg, context=None):
     fields = fields[:]
@@ -47,14 +29,12 @@ def salesinq(obj, cr, uid, ids, fields, arg, context=None):
             if not valid_si_code:
                 result[product_id][fld] = ''
                 continue
-            #result[product_id][fld] = 'fake html'   # TODO remove for live install!!
-            #continue                                # this line too!
-            htmlContentList = [ ]
-            for shortname, longname, SalesInqURL in salesinq_links:
-                if shortname == 'salesinq_allyears_rep':
-                    htmlContentList.append('<br>')
-                htmlContentList.append('''<a href="javascript:ajaxpage('%s','salesinqcontent');">&bullet;%s&bullet;&nbsp;</a>''' % (SalesInqURL % si_code, longname))
-            #for ii in htmlContentList: print ii
+            htmlContentList = []
+            if len(salesinq_links) > 1:
+                for shortname, longname, SalesInqURL in salesinq_links:
+                    if shortname == 'salesinq_allyears_rep':
+                        htmlContentList.append('<br>')
+                    htmlContentList.append('''<a href="javascript:ajaxpage('%s','salesinqcontent');">&bullet;%s&bullet;&nbsp;</a>''' % (SalesInqURL % si_code, longname))
             htmlContentList.append('''
                     <div id="salesinqcontent"></div>
                     <script type="text/javascript">
@@ -83,42 +63,12 @@ class product_product(osv.Model):
             type='boolean',
             method=False,
             ),
-        #'salesinq_allyrs_shipto': fields.function(
-        #    _salesinq,
-        #    string='SalesInq All Years by Ship To',
-        #    type='html',
-        #    method=False,
-        #    ),
-        #'salesinq_allyrs_statecity': fields.function(
-        #    _salesinq,
-        #    string='SalesInq All Years by State/City',
-        #    type='html',
-        #    method=False,
-        #    ),
-        #'salesinq_allyrs_citycust': fields.function(
-        #    _salesinq,
-        #    string='SalesInq All Years by City/Customer',
-        #    type='html',
-        #    method=False,
-        #    ),
-        #'salesinq_allyrs_repcust': fields.function(
-        #    _salesinq,
-        #    string='SalesInq All Years by Rep/Customer',
-        #    type='html',
-        #    method=False,
-        #    ),
-        'salesinq_allyrsmos': fields.function(
+        'salesinq_data': fields.function(
             salesinq,
             multi='salesinq',
             string='SalesInq All Years & Months',
             type='html',
             method=False,
             ),
-        #'salesinq_allyrsmos_shipto': fields.function(
-        #    _salesinq,
-        #    string='SalesInq All Years & Months by Ship To',
-        #    type='html',
-        #    method=False,
-        #    ),
         }
 product_product()
