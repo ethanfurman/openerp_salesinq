a8d0de614838c22b8aad9d4c374e52240d013f28
Ethan Furman <ethan@stoneleaf.us>
2014-10-14 19:05:46 -0700

    add missing '.id' to parent_id reference

--- res_partner.py
+++ res_partner.py
@@ -39,7 +39,7 @@ def salesinq(obj, cr, uid, ids, fields, arg, context=None):
         if parent_id is not None and not valid_si_code:
             si_code = xml_ids[parent_id]
             valid_si_code = is_valid(si_code)
-            partner = obj.browse(cr, uid, [parent_id], context=context)[0]
+            partner = obj.browse(cr, uid, [parent_id.id], context=context)[0]
         result[partner_id]['is_salesinq_able'] = valid_si_code
         for fld in fields:
             if not valid_si_code:
